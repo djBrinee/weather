2bdc32edf62c625497a875c992ae9486def967f6
djBrinee <deivyjunior12@gmail.com>
2023-06-28 22:48:07 -0400

    adding comments with sense

--- lib/backend.dart
+++ lib/backend.dart
@@ -5,36 +5,27 @@ class Backend_Weather {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Test if location services are enabled.
+    //Validating service is enable
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
+    //If the service is enabled, the code will validate the permission about location in Android
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
+      //If permission is denied, the code will ask for it
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return Future.error('Location permissions are denied');
       }
     }
-
+    // If permission is denied forever, the code will never work
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
+    // If permission is allowed and the service is enabled, the code will return current position (latitude and longitude)
     return await Geolocator.getCurrentPosition();
   }
 }
