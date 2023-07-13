238c40ad518d79506776952f93c7a665543e3fe0
djBrinee <deivyjunior12@gmail.com>
2023-07-12 22:14:35 -0400

    adding image

--- lib/Screens/city_screen.dart
+++ lib/Screens/city_screen.dart
@@ -22,45 +22,51 @@ class CityScreen extends StatelessWidget {
                 Navigator.pop(context);
               }),
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                  onChanged: (text) {
-                    city = text;
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/city_background.jpg'), fit: BoxFit.cover)),
+          constraints: BoxConstraints.expand(),
+          child: Padding(
+            padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                    onChanged: (text) {
+                      city = text;
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your city name',
+                    )),
+                GestureDetector(
+                  onTap: () async {
+                    var oNetWorking = Networking(url: getUrlForSearched(city));
+                    var weatherData = await oNetWorking.getWeatherSearched();
+                    Navigator.pop(context, weatherData);
                   },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter your city name',
-                  )),
-              GestureDetector(
-                onTap: () async {
-                  var oNetWorking = Networking(url: getUrlForSearched(city));
-                  var weatherData = await oNetWorking.getWeatherSearched();
-                  Navigator.pop(context, weatherData);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Container(
-                    decoration:
-                        BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-                    width: 200,
-                    height: 50,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Search",
-                          style: TextStyle(fontSize: 30),
-                        ),
-                      ],
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+                      width: 200,
+                      height: 50,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Search",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
